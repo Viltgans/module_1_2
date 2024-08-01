c476b823ac318f170aa22712e6e294149c701651
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-01 11:24:31 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,10 @@
+name = 'Python'
+name_text = 'Курс: '
+task_count = 12
+task_count_text = 'всего задач:'
+time = 1.5
+time_text = 'затрачено часов:'
+time_on_task = time / task_count
+wasted_time = 'среднее время выполнения:'
+space = ','
+print(name_text + name + space, task_count_text, task_count, space, time_text, time, space, wasted_time, time_on_task)
